42be5daf5a5d2b610ac8bf5955dc4867a0cdc5e1
PedroBastosBS <pedrobastos750@gmail.com>
2024-09-19 10:51:14 -0400

    feat: add middleware

--- app/Modules/User/Http/Middlewares/Cors.php
+++ app/Modules/User/Http/Middlewares/Cors.php
@@ -7,11 +7,21 @@ use Illuminate\Http\Request;
 
 class Cors
 {
+     /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+
+        return $response;
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Modules\User\Http\Middlewares\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->append(\App\Modules\User\Http\Middlewares\Cors::class);
+        $middleware->pushMiddleware(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
