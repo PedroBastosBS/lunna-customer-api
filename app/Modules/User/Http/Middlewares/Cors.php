32f89c945d6c0397e945812e5de5d6aa9ed86a0a
PedroBastosBS <pedrobastos750@gmail.com>
2024-09-19 11:26:09 -0400

    fix: ajuste middleware

--- app/Modules/User/Http/Middlewares/Cors.php
+++ app/Modules/User/Http/Middlewares/Cors.php
@@ -16,11 +16,21 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'     => 'Content-Type, X-Auth-Token, Origin, Authorization, X-User-Agent',
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('OK', 204, $headers);
+        }
+
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization, X-User-Agent');
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
 
         return $response;
     }
