8d6a0650db9a3c97716b8ab42f6ff6be71d98004
PedroBastosBS <pedrobastos750@gmail.com>
2024-09-19 20:02:48 -0400

    fix: porra de cors

---
+++ app/Modules/User/Http/Middlewares/Cors.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Modules\User\Http\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+     /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization, X-User-Agent, Accept',
+        ];
+
+        $response = $next($request);
+
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+        return $response;
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Modules\User\Http\Middlewares\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->pushMiddleware(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
