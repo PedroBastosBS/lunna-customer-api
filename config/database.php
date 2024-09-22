0c44ca2b4f491904d175cca309c119f26ca31ad7
PedroBastosBS <pedrobastos750@gmail.com>
2024-09-22 19:37:07 -0400

    feat: alterando host

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST_TESTE', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
