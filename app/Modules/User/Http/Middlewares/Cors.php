0b64b43a2b6756a2f8f8e54071c25bfb410d67b9
PedroBastosBS <pedrobastos750@gmail.com>
2024-09-22 18:01:10 -0400

    feat: ajuste middleware

--- app/Modules/User/Http/Middlewares/Cors.php
+++ app/Modules/User/Http/Middlewares/Cors.php
@@ -16,17 +16,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization, X-User-Agent, Accept',
-        ];
-
         $response = $next($request);
 
-        foreach ($headers as $key => $value) {
-            $response->headers->set($key, $value);
-        }
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, X-User-Agent, Accept');
+    
         return $response;
     }
 }
