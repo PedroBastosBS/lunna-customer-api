95a2921ebd6d65a09e00ffa5b5b02bfd30d11759
PedroBastosBS <pedrobastos750@gmail.com>
2024-09-22 18:05:09 -0400

    feat: add middleware para cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Modules\User\Http\Middlewares\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->append(Cors::class);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
