5c068a136e5e045a030f1b49f287fd7ea998c013
PedroBastosBS <pedrobastos750@gmail.com>
2024-10-03 19:21:51 -0400

    chore: cors config

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
-    
+    */
     'paths' => ['api/'],
     'allowed_methods' => ['POST', 'GET', 'OPTIONS', 'PUT'],
     'allowed_origins' => ['http://localhost:3000'],
@@ -22,5 +22,5 @@ return [
     'exposed_headers' => [],
     'max_age' => 86400,
     'supports_credentials' => false,
-    */
+    
 ];
