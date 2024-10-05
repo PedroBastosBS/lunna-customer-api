3aeb0542b361b55f89b7c4024290bc9259e30564
eliellima00 <eliellimamatos@gmail.com>
2024-10-05 14:26:58 -0400

    feat: cors

--- config/cors.php
+++ config/cors.php
@@ -14,11 +14,11 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-    'paths' => ['api/'],
-    'allowed_methods' => ['POST', 'GET', 'OPTIONS', 'PUT'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'paths' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Origin, Content-Type, Authorization, X-User-Agent, Accept'],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 86400,
     'supports_credentials' => false,
